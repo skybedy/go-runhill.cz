fec8b4cc11dcbe5389ee0bd5b0ac92fc8c509c28
skybedy <skybedy@gmail.com>
2021-03-12 21:01:23 +0100

    ...

--- main.go
+++ main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"runhill.cz/routes"
 )
-
-func neco(c *gin.Context) {
-	bla := c.Param("bla")
-	c.JSON(200, gin.H{
-		"message": bla,
-	})
-}
 
 func main() {
 	router := gin.Default()
-
-	router.GET("/", neco)
-
-	router.GET("/user/:name", func(c *gin.Context) {
-		//name := c.Param("name")
-		firstname := c.DefaultQuery("firstname", "Guest")
-		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
-
-		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
-	})
-
-	router.GET("/ping/:bla", func(c *gin.Context) {
-		x := c.Param("bla")
-		c.JSON(200, gin.H{
-
-			"message": x,
-		})
-	})
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", routes.Index)
 	router.Run(":1305")
 }
